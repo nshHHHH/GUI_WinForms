448de19ff9dd4e3b24a87c3193e701912d7f649a
Никита Шубенкин <n.shubenkin@mail.ru>
2022-12-05 19:00:11 +0500

    Добавьте файлы проекта.

---
+++ GUI_WinForms/MyForm.cpp
@@ -0,0 +1,6 @@
+#include "MyForm.h"
+int main()
+{
+	System::Windows::Forms::Application::Run(gcnew GUIWinForms::MyForm);
+	return 0;
+}
---
+++ GUI_WinForms/MyForm.h
@@ -0,0 +1,220 @@
+#pragma once
+#include <math.h>
+#include <Windows.h>
+#include <stdio.h>
+namespace GUIWinForms {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System::Collections::Generic;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+			graphics = this->pictureBox1->CreateGraphics();
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	protected:
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Timer^ timer1;
+	private: System::ComponentModel::IContainer^ components;
+		   Graphics^ graphics;
+		   int  x, y,i;
+		   double angle;
+		   Pen^ pen;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+
+		   Random^ random;
+		   
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(1, 3);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(663, 507);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
+			// 
+			// button1
+			// 
+			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->button1->Location = System::Drawing::Point(2, 2);
+			this->button1->Margin = System::Windows::Forms::Padding(2);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(60, 185);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Start";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// timer1
+			// 
+			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm::timer1_Tick);
+			// 
+			// button2
+			// 
+			this->button2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->button2->Location = System::Drawing::Point(2, 191);
+			this->button2->Margin = System::Windows::Forms::Padding(2);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(60, 185);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Restart";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(737, 12);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(703, 15);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(14, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"P";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(360, 15);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(12, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"y";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(621, 191);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(12, 13);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"x";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(849, 535);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->timer1->Enabled = true;
+	}
+		   private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+			   Application::Restart();
+
+
+		   }
+	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+	
+		
+	}
+	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
+		int m_x[200];
+		int m_y[200];
+		for (int i1 = 0; i1 < 200; i1++) {
+			m_x[i1] = 0;
+			m_y[i1] = 0;
+		}
+		double p = Convert::ToDouble(textBox1->Text);
+		for (i = 0; i < 150; i++) {
+			x = x + 1;
+			m_x[i] = x;
+			y = Convert::ToInt32(sqrt((double)(2 *p* x)));//вычисл€ем y 
+			m_y[i] = y;
+		}
+		for (int j = 0; j < 199; j++) {
+			Graphics^ formGraphics = this->pictureBox1->CreateGraphics();
+			formGraphics->FillRectangle(System::Drawing::Pens::Red->Brush, m_x[j] + 200, m_y[j] + 200, 1, 1);//одно значение y
+			formGraphics->FillRectangle(System::Drawing::Pens::Red->Brush, m_x[j] + 200, 0 - m_y[j] + 200, 1, 1);// второе значение y
+		}
+	}
+	private: System::Void chart1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
